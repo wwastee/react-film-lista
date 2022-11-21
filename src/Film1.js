cc697974fa59c534c3275ff1fc7db85d1dddc728
fraberto001@gmail.com <fraberto001@gmail.com>
2022-11-21 16:56:47 +0100

    incasinamenti, devo portare funziione da un lato all'altro

--- src/Film1.js
+++ src/Film1.js
@@ -1,19 +1,28 @@
-import Titolo1 from "./Comp1/Titolo1"
-import Immagine1 from "./Comp1/Immagine1"
-import Descrizione1 from "./Comp1/Descrizione1"
-import Bottone1 from "./Comp1/Bottone1"
+import Titolo1 from "./Comp1/Titolo1";
+import Immagine1 from "./Comp1/Immagine1";
+import Descrizione1 from "./Comp1/Descrizione1";
+import Bottone1 from "./Comp1/Bottone1";
+import { useState } from "react";
+
+function Film1() {
+  
+    const [contatore, PicchiamentoBarbone] = useState(0);
+    function giannino(){
+        PicchiamentoBarbone(contatore+1)
+    }
 
 
-function Film1(){
-    return(
-        <div className="cards">
-        <Titolo1></Titolo1>
-        <Immagine1></Immagine1>
-        <Descrizione1></Descrizione1>
-        <Bottone1></Bottone1>
-
-        </div>
-    )
+  
+  return (
+    <div className="cards">
+      <Titolo1></Titolo1>
+      <Immagine1></Immagine1>
+      <Descrizione1></Descrizione1>
+      <Bottone1 {giannino}></Bottone1>
+      {/*<button onClick={() => PicchiamentoBarbone(contatore + 1)}></button>*/}
+      <p> you clicked {contatore} volte</p>
+    </div>
+  );
 }
 
-export default Film1
+export default Film1;
